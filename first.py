c3035a9932b1ec9d78611a3dab0b5868d4be7288
AntonioSiciliano <78103925+AntonioSiciliano@users.noreply.github.com>
2021-02-03 10:44:43 +0100

    Add files via upload

---
+++ first.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+from scipy import optimize
+import numdifftools as nd
+from timeit import default_timer as timer
+
+print("Hello word")
+
